f750dada49216064bce193b5b2cd9d389560c3c1
Amberlie Porter <whenamesattacks@gmail.com>
2022-09-22 21:34:39 -0700

    stopping conflicts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,54 +1,54 @@
-const router = require('express').Router();
-const { User } = require('../../models')
+// const router = require('express').Router();
+// const { User } = require('../../models')
 
-// Login copy from 19-middleware
-router.post('/login', async (req, res) => {
-    try {
-      const dbUserData = await User.findOne({
-        where: {
-          email: req.body.email,
-        },
-      });
+// // // Login copy from 19-middleware
+// // router.post('/login', async (req, res) => {
+//     try {
+//       const dbUserData = await User.findOne({
+//         where: {
+//           email: req.body.email,
+//         },
+//       });
   
-      if (!dbUserData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password. Please try again!' });
-        return;
-      }
+//       if (!dbUserData) {
+//         res
+//           .status(400)
+//           .json({ message: 'Incorrect email or password. Please try again!' });
+//         return;
+//       }
   
-      const validPassword = await dbUserData.checkPassword(req.body.password);
+//       const validPassword = await dbUserData.checkPassword(req.body.password);
   
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password. Please try again!' });
-        return;
-      }
+//       if (!validPassword) {
+//         res
+//           .status(400)
+//           .json({ message: 'Incorrect email or password. Please try again!' });
+//         return;
+//       }
   
-      req.session.save(() => {
-        req.session.loggedIn = true;
+//       req.session.save(() => {
+//         req.session.loggedIn = true;
   
-        res
-          .status(200)
-          .json({ user: dbUserData, message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+//         res
+//           .status(200)
+//           .json({ user: dbUserData, message: 'You are now logged in!' });
+//       });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   });
   
-  // Logout
-  router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
+//   // Logout
+//   router.post('/logout', (req, res) => {
+//     if (req.session.loggedIn) {
+//       req.session.destroy(() => {
+//         res.status(204).end();
+//       });
+//     } else {
+//       res.status(404).end();
+//     }
+//   });
   
-  module.exports = router;
+//   module.exports = router;
   
