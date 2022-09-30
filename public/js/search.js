3e2aae1475ab29da3a99f9dc51c3189e37e47b11
Gina Marie <ginamarie214@gmail.com>
2022-09-29 20:50:22 -0500

    search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,4 @@
 const searchBar = async (event) => {
     event.preventDefault();
 
-    const searchable = document.querySelector('#searching').value.trim();
-}
+    const searchable = document.querySelector('#searching').value.trim();}
