044ec9a2064403d1bfc336cadb982de743210b14
Gina Marie <ginamarie214@gmail.com>
2022-09-30 00:06:14 -0500

    updated git ignore for heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,3 @@
-// this is copy and pasted from middleware lesson (week 14, lesson 20)
-// to be modified as needed
-
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
